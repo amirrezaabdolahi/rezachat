b7d0bff259838c0f2556b353150b2f259ddb2540
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-01-12 17:26:19 +0330

    make some changes

--- frontend/components/components/ChatInput.jsx
+++ frontend/components/components/ChatInput.jsx
@@ -1,16 +1,48 @@
+"use client";
+import { messagesData } from "@/fakeDatas";
 import { Send } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const ChatInput = () => {
+    const [inputMessage, setInputMessage] = useState("");
+    const currentChat = useSelector((state) => state.chat.selectedChat);
+    const filteredChat = messagesData.data.filter(
+        (chat) => chat.id === Number(currentChat)
+    );
+
+    const handleSendMessage = async () => {
+
+
+        console.log(filteredChat[0].messages);
+
+        filteredChat[0].messages.push({
+            id: filteredChat[0].messages.length + 1,
+            chat: 4,
+            sender: "root",
+            message: inputMessage,
+            created_at: new Date().toLocaleString(),
+        });
+    };
+
     return (
         <div className="w-full flex gap-2 h-full p-2 ">
             <div className=" w-full h-full bg-white rounded-full text-end flex items-center p-2">
-                <input className="w-full h-full text-black outline-0" />
+                <input
+                    className="w-full h-full text-black outline-0"
+                    value={inputMessage}
+                    onChange={(e) => {
+                        setInputMessage(e.target.value);
+                    }}
+                />
             </div>
-            <div className="bg-white text-black w-20 h-full rounded-full flex items-center justify-center cursor-pointer">
+            <button
+                onClick={handleSendMessage}
+                className="bg-white hover:bg-white/70 active:bg-white/80 active:scale-[0.9] transition-all text-black w-20 h-full rounded-full flex items-center justify-center cursor-pointer"
+            >
                 <Send />
-            </div>
-        </div>  
+            </button>
+        </div>
     );
 };
 
